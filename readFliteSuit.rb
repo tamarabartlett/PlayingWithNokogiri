743ce401e7d4668daa83ae8c44f34953909e5799
Tamara Bartlett <tbartlet@thoughtworks.com>
2016-07-05 14:51:25 -0700

    Lots of stealing to make a react thing work on my computer

--- readFliteSuit.rb
+++ readFliteSuit.rb
@@ -1,12 +1,10 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-FLITESUIT_URL = "http://www.flitesuit.com"
+FLITESUIT_URL = "http://www.flitesuit.com/contact.html"
 page = Nokogiri::HTML(open(FLITESUIT_URL ))
 puts page.class   # => Nokogiri::HTML::Document
 
-print(page)
-
-open('flitesuit.html', 'w') { |f|
+open('flitesuit-contact.html', 'w') { |f|
   f.puts page
 }

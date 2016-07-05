59c68ae268fdfec282b0d495b6e1e92ee3984838
Tamara Bartlett <tbartlet@thoughtworks.com>
2016-07-05 15:43:42 -0700

    adds state to HelloUser class

--- react-15.2.0/src/helloworld.js
+++ react-15.2.0/src/helloworld.js
@@ -8,4 +8,19 @@ var HelloWorld = React.createClass({
   }
 });
 
-ReactDOM.render(<HelloWorld />, document.getElementById('example'));
+var HelloUser = React.createClass({
+  getInitialState: function(){
+    return {
+      username: 'namarab'
+    }
+  },
+  render: function(){
+    return (
+      <div>
+        Hello {this.state.username}
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<HelloUser />, document.getElementById('example'));
